7c1680fd05e0a236ce3ed59c286073b98489d187
ashu <aashuk012@gmail.com>
2021-01-03 21:25:34 +0530

    chnage2

--- satabazer/settings.py
+++ satabazer/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'blogdb',
-        'USER': 'blog_admin',
+        'USER': 'admin',
         'PASSWORD': 'testing123',
         'HOST': 'localhost',
         'PORT': '',
