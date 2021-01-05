2a1ba6f47a0fe52183f1b8f4c0fde63843bab437
ashu <aashuk012@gmail.com>
2021-01-05 18:26:06 +0530

    lkk

--- satabazer/settings.py
+++ satabazer/settings.py
@@ -136,7 +136,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static_in_env')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 
 
