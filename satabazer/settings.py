1bb94a3e79f3ac064a2feb8f0a9904725ae2e66f
ashu <aashuk012@gmail.com>
2021-03-08 15:09:50 +0530

    change ip

--- satabazer/settings.py
+++ satabazer/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'i8!w1hv)hr_q_4(o)r_*if9x&ek^q*9$7_lwl4dgq3w2fpp-^%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['167.71.181.209']
 
 
 # Application definition
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'satabazer.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'blogdb',
+        'USER': 'blog_admin',
+        'PASSWORD': 'testing123',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
